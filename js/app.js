e30e38974709519bba31310f3ff6df7a03d0a2b1
Ryley Ameden <ryley@ameden.me>
2016-12-11 09:03:37 -0500

    Update 2016-12-11T14:03:35.934Z

--- js/app.js
+++ js/app.js
@@ -10345,8 +10345,8 @@ return jQuery;
 
 }));
 jQuery(document).ready(function($){
-  $("header").load("/dist/templates/header.php");
-  $("#intro").load("/dist/templates/intro.php");
+  $("header").load("/templates/header.php");
+  $("#intro").load("/templates/intro.php");
 });
 
 /*! UIkit 2.27.2 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
