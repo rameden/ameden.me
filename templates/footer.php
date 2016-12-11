c4be59560ce1452f88a8ffe7abf076585d6dbf94
Ryley Ameden <ryley@ameden.me>
2016-12-11 09:37:33 -0500

    Update 2016-12-11T14:37:31.255Z

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <div id="about-modal" class="uk-modal">
-    <div class="uk-modal-dialog uk-animation-scale">
+    <div class="uk-modal-dialog uk-animation-slide-bottom">
         <a class="uk-modal-close uk-close"></a>
         ...
     </div>
