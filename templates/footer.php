4e90e2f4a203e502f1ce494292ae49d82a60460c
Ryley Ameden <ryley@ameden.me>
2016-12-11 08:39:46 -0500

    Update 2016-12-11T13:39:44.884Z

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 <div id="about-modal" class="uk-modal">
     <div class="uk-modal-dialog uk-animation-slide-bottom">
         <a class="uk-modal-close uk-close"></a>
-        ...
+        <img class="uk-border-circle" src="css/images/head.jpg" alt="" width="100">
     </div>
 </div>
