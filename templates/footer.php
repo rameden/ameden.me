ec69209bdb193e8a977702f1a03ebffe187ca4c8
Ryley Ameden <ramenden@dhcp-146.office.mht.dyndns.com>
2016-12-13 12:30:12 -0500

    Update 2016-12-13T17:30:11.269Z

--- templates/footer.php
+++ templates/footer.php
@@ -31,15 +31,15 @@
           </div>
           <div class="modal-intro uk-margin-medium-top uk-grid">
             <div class="uk-width-1-1 uk-margin-medium-bottom">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex</p>
+              <p>I modernize, customize and other ‘ize-y things at <a href="http://dyn.com">Dyn</a>! My background involves pushing the limits of of what we can build on the backend and how we can experience it on the frontend. My passion is finding the best way to complete a project, optimization and teaching those around me.</p>
             </div>
             <div class="uk-width-small-1-1 uk-width-medium-1-2">
-              <h4>A title goes here!</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
+              <h4>Working Together?</h4>
+              <p>My motto is: “Helping everyday business with development and marketing” Sound good? <a href="mailto:ryley@ameden.me">Contact me</a>.</p>
             </div>
             <div class="uk-width-small-1-1 uk-width-medium-1-2">
-              <h4>A title goes here!</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
+              <h4>How'd you build this site?!</h4>
+              <p>Because I love open-source it's available for you to see. Find a bug? Report it!<a href="https://github.com/rameden/ameden.me"> View source</a>.</p>
             </div>
           </div>
           <div class="uk-text-muted uk-text-center uk-margin-medium-top">Copyright © 2016 Ryley Ameden. MTFBWU</div>
